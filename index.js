c3b169a008309d494e85809cf78b94fd91f72759
Yampoy <yampoy.elvis@gmail.com>
2025-03-08 23:08:44 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+// returnFirstTwoDrivers function
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+};
+
+// returnLastTwoDrivers function
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+};
+
+// selectingDrivers array
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// createFareMultiplier function
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+        return fare * multiplier;
+    };
+}
+
+// fareDoubler function
+const fareDoubler = createFareMultiplier(2);
+
+// fareTripler function
+const fareTripler = createFareMultiplier(3);
+
+// selectDifferentDrivers function
+function selectDifferentDrivers(arrayOfDrivers, fn) {
+    return fn(arrayOfDrivers);
+}
